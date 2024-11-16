59108f7a006c27e94b438304872e33619fded861
Mhassam1100 <166863435+Mhassam1100@users.noreply.github.com>
2024-11-17 04:13:45 +0500

    Add files via upload

---
+++ src/app/main/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function main() {
+  return 
+
+
+  (
+    
+    <div className="bg-black p-6 rounded-lg shadow-lg">
+    
+    <p className="text-gray-600">Short description about the service or feature.</p>
+    </div>
+  );
+}
